65038163284d4457ff617accd8afe5cb99141e25
Miguelsre10 <91726348+Miguelsre10@users.noreply.github.com>
2022-05-06 12:59:50 +0200

    giro prueba mejora

--- main.ts
+++ main.ts
@@ -1,28 +1,43 @@
+let Dir = 0
+function BuscaObjetivo (núm: number) {
+	
+}
 function DetectaBorde () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 || maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         Esquiva()
-        Gira(randint(-180, 180))
+        Gira(randint(0, 180))
     } else {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
     }
 }
 function Gira (TiempoGiro: number) {
-    if (TiempoGiro > 0) {
+    Dir = TiempoGiro
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
-        basic.pause(Math.map(Math.abs(TiempoGiro), -180, 180, 0, 506))
+        basic.pause(Math.map(Math.abs(Dir), 0, 180, 0, 506))
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        basic.pause(20)
+        basic.pause(TiempoGiro)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        basic.pause(Math.map(Math.abs(Dir), 0, 180, 0, 506))
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        basic.pause(TiempoGiro)
     } else {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        basic.pause(Math.map(Math.abs(TiempoGiro), -180, 180, 0, 506))
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
-        basic.pause(20)
+        if (Math.map(Dir, 0, 180, -5, 4) >= 0) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+            basic.pause(TiempoGiro)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+            basic.pause(TiempoGiro)
+        }
     }
 }
 function Esquiva () {
