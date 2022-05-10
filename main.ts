c9c025e2925e118a7ce42e02875408a34c3313c8
Miguelsre10 <91726348+Miguelsre10@users.noreply.github.com>
2022-05-10 11:27:24 +0200

    correccion velocidad

--- main.ts
+++ main.ts
@@ -1,48 +1,50 @@
-let Dir = 0
-function BuscaObjetivo (núm: number) {
-	
-}
+let LineaI = 0
+let LineaD = 0
+let Aux = 0
 function DetectaBorde () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        LineaI = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+        LineaD = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
         Esquiva()
-        Gira(randint(0, 180))
+        Gira(randint(60, 506))
     } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 175)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     }
 }
 function Gira (TiempoGiro: number) {
-    Dir = TiempoGiro
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+    if (LineaI == 1 && LineaD == 0) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
-        basic.pause(Math.map(Math.abs(Dir), 0, 180, 0, 506))
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        basic.pause(TiempoGiro)
+    } else if (LineaI == 0 && LineaD == 1) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         basic.pause(TiempoGiro)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        basic.pause(Math.map(Math.abs(Dir), 0, 180, 0, 506))
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
-        basic.pause(TiempoGiro)
-    } else {
-        if (Math.map(Dir, 0, 180, -5, 4) >= 0) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-            basic.pause(TiempoGiro)
-        } else {
+    } else if (LineaD == 1 && LineaI == 1) {
+        Aux = randint(0, 1)
+        if (Aux == 0) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+            basic.pause(TiempoGiro)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
             basic.pause(TiempoGiro)
         }
     }
 }
 function Esquiva () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 200)
-    basic.pause(500)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+    basic.pause(400)
 }
 basic.forever(function () {
     DetectaBorde()
